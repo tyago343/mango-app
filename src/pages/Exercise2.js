f765a181b596111b03d0844a22d9dfd554113af3
tyago343 <tyagocasanova@gmail.com>
2021-09-09 13:53:18 +0200

    Agregada la llamada en exercise2

--- src/pages/Exercise2.js
+++ src/pages/Exercise2.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getFixedRanges } from '../utils/RangesApi';
+import Range from '../components/Range';
+
 
 function Exercise2() {
+  const [fixedRanges, setFixedRanges] = useState([]);
+  useEffect(()=> {
+    const { fixedRanges } = getFixedRanges();
+    setFixedRanges(fixedRanges)
+  }, [])
   return (
-    <h1>Goodbye world</h1>
+    <Range fixedRanges={fixedRanges} />
   );
 }
 export default Exercise2;
